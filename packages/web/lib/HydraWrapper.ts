4f808cf3917c89e15f4f26d108e0441dde699721
solquemal <solquemal@gmail.com>
2021-08-16 00:39:49 -0300

    agregar POSE

--- packages/web/lib/HydraWrapper.ts
+++ packages/web/lib/HydraWrapper.ts
@@ -8,7 +8,37 @@ class HydraWrapper {
     // eslint-disable-next-line global-require
     const Hydra = require("hydra-synth");
     // eslint-disable-next-line global-require
-    const P5 = require("./p5-wrapper");
+      const P5 = require("./p5-wrapper");
+      require('@tensorflow/tfjs-core');
+      require('@tensorflow/tfjs-converter');
+      require('@tensorflow/tfjs-backend-webgl');
+
+      const posenet = require('@tensorflow-models/posenet');
+
+
+      async function estimatePoseOnImage(imageElement) {
+          // load the posenet model from a checkpoint
+          const net = await posenet.load(P5.canvas);
+
+          const pose = await net.estimateSinglePose(imageElement, {
+              flipHorizontal: false
+          });
+          return pose;
+      }
+
+      p = new P5()
+      p.createCanvas(100, 100);
+      capture = p.createCapture(
+          p.VIDEO
+      );
+      capture.hide();
+
+
+      const pose = async () => await estimatePoseOnImage(p.canvas);
+
+      window.pose = pose;
+
+      window.worker=setInterval( async () => window.score = await pose(), 500 )
 
     window.P5 = P5;
 
